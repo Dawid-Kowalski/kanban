ad2d09b7ea1f77d2bf66c92846f472d88ab082f3
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2018-12-15 14:47:43 +0100

    Add mustahe and simply template for columns

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
 
 	let uniqueID = 0;
     
+    data = 	{
+    			id: 10
+    		}
+
+
     function generateID () {
     	uniqueID++;
     	return uniqueID;
     }
 
-    
+ 	let template = document.getElementById("template-column").innerHTML;
+ 	let columns = document.getElementById("columns");
+  	let element = document.createElement("div");
+
+  	Mustache.parse(template);
+  	element.innerHTML = Mustache.render(template, data);
+
+  	columns.appendChild(element);
 });
